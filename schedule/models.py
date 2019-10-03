896fc4584b600b566c71edb923c8cbf36777e98c
Max Thyron <mkthyron@gmail.com>
2019-10-03 13:38:43 +0300

    Added regex for auditorium processing + All seems to be working

--- schedule/models.py
+++ schedule/models.py
@@ -23,10 +23,10 @@ class ScheduleTime(models.Model):
 
 
 class ScheduleSubject(models.Model):
-    type = models.CharField(max_length=10, null=True, blank=True)
-    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=150)
     auditorium = models.ForeignKey(Auditorium, on_delete=models.PROTECT)
-    professor = models.CharField(max_length=50, null=True, blank=True)
+    professor = models.CharField(max_length=150, null=True, blank=True)
     time = models.ForeignKey(ScheduleTime, on_delete=models.PROTECT)
     week_interval = models.IntegerField()
     day = models.IntegerField()
