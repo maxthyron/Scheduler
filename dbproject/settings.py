a511ea7fc04b46925915f84927763712b5aa201f
Max Thyron <mkthyron@gmail.com>
2019-10-14 16:06:09 +0300

    Settings updated

--- dbproject/settings.py
+++ dbproject/settings.py
@@ -74,24 +74,18 @@ WSGI_APPLICATION = 'dbproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE':       'django.db.backends.sqlite3',
-        'NAME':         os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':       'django.db.backends.postgresql_psycopg2',
+        'NAME':         'schedule_db',
+        'USER':         'scheduler_usr',
+        'PASSWORD':     'Postgres08',
+        'HOST':         '127.0.0.1',
+        'CONN_MAX_AGE': 500,
+        'PORT':         '5432',
         }
     }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE':       'django.db.backends.postgresql_psycopg2',
-#         'NAME':         'schedule_db',
-#         'USER':         'scheduler_usr',
-#         'PASSWORD':     'Postgres08',
-#         'HOST':         '127.0.0.1',
-#         'CONN_MAX_AGE': 500,
-#         'PORT':         '5432',
-#         }
-#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
